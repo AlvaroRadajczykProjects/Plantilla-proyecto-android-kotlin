d2dac04c261b8a939823ba1d9dd7dd5d7ec1baa6
Álvaro Radajczyk <40389952+AlvaroRadajczykProjects@users.noreply.github.com>
2023-09-21 23:37:09 +0200

    create all fragments and navigator

--- app/src/main/java/com/example/plantillaproyectoandroidkotlin/fragments/FragmentNext.kt
+++ app/src/main/java/com/example/plantillaproyectoandroidkotlin/fragments/FragmentNext.kt
@@ -6,9 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.plantillaproyectoandroidkotlin.R
-import com.example.plantillaproyectoandroidkotlin.databinding.FragmentMainBinding
 
-class FragmentMain : Fragment() {
+class FragmentNext : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +17,8 @@ class FragmentMain : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_next, container, false)
     }
+
 }
